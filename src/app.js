1c242cbf12cf64cde0cb4ed9ff8ba865f19e9393
kkang <kkang@student.42seoul.kr>
2021-07-14 02:15:46 +0900

    [NP-26] Install Webpack
    - 웹팩 설치, entry, output 설정 -> webpack.config.js
    - 테스트를 위한 src 파일 추가

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import * as math from "./math.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.querySelector("#btn");
+  const result = document.querySelector("#result");
+
+  btn.addEventListener("click", () => {
+    result.textContent = math.sum(1, 2);
+
+    console.log(result.textContent);
+  });
+});
