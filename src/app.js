86a8fcfd20c4d123c8e2fd287cd04389b1676637
kkang <kkang@student.42seoul.kr>
2021-08-12 01:47:07 +0900

    [NP-38] 버튼 클릭 시 계속 누르면 빠르게 값이 변하는 기능 구현
    - moudedown event를 listen
      1) event가 발생 버튼과 같은 부모를 갖는 input 값을 추출
      2) 누른 상태를 관리하는 isPressed 의 상태 변경
      3) changeValue 함수 호출
    
    - mouseup event listen
      - mouseup 발생 시, isPressed의 상태 변경
      -> changeValue 내에서 값 변경이 이루어지지 않도록 함
    
    - changeValue
      - settimeout 이용, 특정 시간 후 input 값을 변경시킴
      - isPressed 상태에 따라 재귀적으로 changeValue 호출

--- src/app.js
+++ src/app.js
@@ -5,6 +5,24 @@ const $ = (selector, scope = document) => scope.querySelector(selector);
 const $$ = (selector, scope = document) => scope.querySelectorAll(selector);
 const on = (target, eventName, handler) => target.addEventListener(eventName, handler);
 
+let isPressed = false;
+let counter = 1000;
+
+document.addEventListener('mouseup', () => {
+  isPressed = false;
+  counter = 1000;
+});
+
+function onMouseUpAddNumberButton() {
+  isPressed = false;
+  counter = 1000;
+}
+
+function onMouseUpSubstractNumberButton() {
+  isPressed = false;
+  counter = 1000;
+}
+
 function returnSpinboxHTMLTemplate() {
   return `
     <div class='spinbox-board'>
@@ -30,20 +48,33 @@ function returnAddSpinboxButtonTemplate() {
   `;
 }
 
-function onClickAddNumberButton(event) {
-  const {target} = event;
-  const $spinbox = target.closest('.spinbox');
-  const $input = $('input', $spinbox);
-
-  $input.value = parseInt($input.value, 10) + 1;
+function changeValue(action, $input) {
+  if (!isPressed) {
+    return;
+  }
+  counter > 35 ? counter /= 1.1 : counter;
+  $input.value = parseInt($input.value, 10) + parseInt(action, 10);
+  setTimeout(() => {
+    changeValue(action, $input);
+  }, counter);
 }
 
-function onClickSubstractNumberButton(event) {
+function onMouseDownAddNumberButton(event) {
   const {target} = event;
   const $spinbox = target.closest('.spinbox');
   const $input = $('input', $spinbox);
 
-  $input.value = parseInt($input.value, 10) - 1;
+  isPressed = true;
+  changeValue(1, $input);
+}
+
+function onMouseDownSubstractNumberButton(event) {
+  const {target} = event;
+  const $spinbox = target.closest('.spinbox');
+  const $input = $('input', $spinbox);
+
+  isPressed = true;
+  changeValue(-1, $input);
 }
 
 function onClickDeleteSpinboxButton(event) {
@@ -67,8 +98,18 @@ function onClickAddSpinboxButton() {
   if (lastSpinbox) {
     lastSpinbox.insertAdjacentHTML('afterend', returnSpinboxHTMLTemplate());
     lastSpinbox = getLastSpinbox();
-    on($('.add-number-button', lastSpinbox), 'click', onClickAddNumberButton);
-    on($('.substract-number-button', lastSpinbox), 'click', onClickSubstractNumberButton);
+    on($('.add-number-button', lastSpinbox), 'mouseup', onMouseUpAddNumberButton);
+    on($('.add-number-button', lastSpinbox), 'mousedown', onMouseDownAddNumberButton);
+    on($('.add-number-button', lastSpinbox), 'mouseleave', () => {
+      isPressed = false;
+      counter = 1000;
+    });
+    on($('.substract-number-button', lastSpinbox), 'mouseup', onMouseUpSubstractNumberButton);
+    on($('.substract-number-button', lastSpinbox), 'mousedown', onMouseDownSubstractNumberButton);
+    on($('.substract-number-button', lastSpinbox), 'mouseleave', () => {
+      isPressed = false;
+      counter = 1000;
+    });
     on($('.delete-spinbox-button', lastSpinbox), 'click', onClickDeleteSpinboxButton);
   } else {
     init();
@@ -78,8 +119,18 @@ function onClickAddSpinboxButton() {
 
 function addEvents() {
   $$('.spinbox-board').forEach(spinbox => {
-    on($('.add-number-button', spinbox), 'click', onClickAddNumberButton);
-    on($('.substract-number-button', spinbox), 'click', onClickSubstractNumberButton);
+    on($('.add-number-button', spinbox), 'mouseup', onMouseUpAddNumberButton);
+    on($('.add-number-button', spinbox), 'mousedown', onMouseDownAddNumberButton);
+    on($('.substract-number-button', spinbox), 'mouseup', onMouseUpSubstractNumberButton);
+    on($('.substract-number-button', spinbox), 'mousedown', onMouseDownSubstractNumberButton);
+    on($('.add-number-button', spinbox), 'mouseleave', () => {
+      isPressed = false;
+      counter = 1000;
+    });
+    on($('.substract-number-button', spinbox), 'mouseleave', () => {
+      isPressed = false;
+      counter = 1000;
+    });
     on($('.delete-spinbox-button', spinbox), 'click', onClickDeleteSpinboxButton);
   });
   on($('#add-spinbox-button'), 'click', onClickAddSpinboxButton);
