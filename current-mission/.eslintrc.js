aa2d9f1bd287672362335eaac425ebedad6a0711
kkang <kyeongmankang@gmail.com>
2021-10-31 23:27:28 +0900

    [NP-85] eslint 설정 추가, 설정에 맞게 코드 수정

--- current-mission/.eslintrc.js
+++ current-mission/.eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
   extends: ['tui/es6', 'prettier'],
   rules: {
     // Override rules or Add more rules
+    'no-new': 'off',
   },
   parserOptions: {
     sourceType: 'module',
