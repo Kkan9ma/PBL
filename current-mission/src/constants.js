0f9da8ceca0298f364eb82365c4001c6fe304c6b
kkang <kyeongmankang@gmail.com>
2021-11-11 11:40:26 +0900

    [NP-100] execCommand 호출 구조 변경
    - CommandsButtonList 에서 사용할 execCommand를 외부에서 주입
    - command의 종류를 용이하게 관리하기 위해 분리하였음.

--- current-mission/src/constants.js
+++ current-mission/src/constants.js
@@ -2,29 +2,50 @@ export const commandsList = [
   {
     command: 'bold',
     text: '진하게',
+    execCommand: (command) => {
+      document.execCommand(command);
+    },
   },
   {
     command: 'italic',
     text: '기울임',
+    execCommand: (command) => {
+      document.execCommand(command);
+    },
   },
   {
     command: 'underline',
     text: '밑줄',
+    execCommand: (command) => {
+      document.execCommand(command);
+    },
   },
   {
     command: 'strikethrough',
     text: '취소선',
+    execCommand: (command) => {
+      document.execCommand(command);
+    },
   },
   {
     command: 'formatBlock',
     text: 'H1',
+    execCommand: (command, text) => {
+      document.execCommand(command, false, text);
+    },
   },
   {
     command: 'formatBlock',
     text: 'H2',
+    execCommand: (command, text) => {
+      document.execCommand(command, false, text);
+    },
   },
   {
     command: 'formatBlock',
     text: 'H3',
+    execCommand: (command, text) => {
+      document.execCommand(command, false, text);
+    },
   },
 ];
