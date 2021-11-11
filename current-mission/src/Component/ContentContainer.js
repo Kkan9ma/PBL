2280e652b6314ef1a4af060b942adebe35c4955c
kkang <kyeongmankang@gmail.com>
2021-11-11 15:45:08 +0900

    [NP-99] 이미지 클릭 시 사이즈 조정할 수 있도록 입력 받는 기능 구현

--- current-mission/src/Component/ContentContainer.js
+++ current-mission/src/Component/ContentContainer.js
@@ -1,4 +1,13 @@
 import React from 'react';
+import { adjustImageTagSize } from '../utils/handleTagElement';
+
+const handleClick = (e) => {
+  const { target } = e;
+
+  if (target.tagName === 'IMG') {
+    adjustImageTagSize(target);
+  }
+};
 
 function ContentContainer() {
   return (
@@ -7,6 +16,7 @@ function ContentContainer() {
         className="editor__content-container"
         style={{ width: '500px', height: '500px', border: '1px solid black' }}
         contentEditable="true"
+        onClick={handleClick}
       ></div>
     </>
   );
