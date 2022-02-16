d3048756ccbd9a653750618b850ee88db1fd4a1d
Kyeongman Kang <kyeongmankang@gmail.com>
2022-02-16 11:49:57 +0900

    [NP-151] 강조 효과를 더하는 로직 구현
    - dom.js: 기능 구현에 필요한 유틸 함수 추가
    - 선택된 range 내 node 추출, text node 판별, querySelector 대체
      - executeTextCommand.js: 선택한 강조표시 적용 여부 확인 로직 추가
        - 유틸 함수 결과를 이용, 원하는 조건에 따라 적용 여부를 판단
        - 적용되어 있지 않았을 경우 surround 하도록 구조 설계
    
    - selection.js: selection api를 이용한 주요 로직이 담긴 파일
      - surroundSelectedRange()
        - 선택한 영역을 div를 구분자로 삼아 여러 range로 나눔
        - 나뉜 여러 개의 range에 각각 강조 표시를 더하는 방식
        - start 지점과 end 지점을 구하기 위해 node 탐색
        - 해당 range에 추가하기 원하는 태그를 재삽입

--- current-mission/src/dom.js
+++ current-mission/src/dom.js
@@ -38,3 +38,60 @@ export function getNodesInRange(range) {
 
   return nodes;
 }
+
+export function nextNode(node) {
+  if (node.hasChildNodes()) {
+    return node.firstChild;
+  } else {
+    while (node && !node.nextSibling) {
+      node = node.parentNode;
+    }
+    if (!node) {
+      return null;
+    }
+    return node.nextSibling;
+  }
+}
+
+export function getRangeSelectedNodes(range) {
+  let node = range.startContainer;
+  const endNode = range.endContainer;
+  const rangeNodes = [];
+
+  // Special case for a range that is contained within a single node
+  if (node === endNode) {
+    return [node];
+  }
+
+  // Iterate nodes until we hit the end container
+  while (node && node !== endNode) {
+    rangeNodes.push((node = nextNode(node)));
+  }
+
+  // Add partially selected nodes at the start of the range
+  node = range.startContainer;
+  while (node && node != range.commonAncestorContainer) {
+    rangeNodes.unshift(node);
+    node = node.parentNode;
+  }
+
+  return rangeNodes;
+}
+
+export function getSelectedNodes() {
+  if (window.getSelection) {
+    const sel = window.getSelection();
+    if (!sel.isCollapsed) {
+      return getRangeSelectedNodes(sel.getRangeAt(0));
+    }
+  }
+  return [];
+}
+
+export function $($target, scope = document) {
+  return scope.querySelector($target);
+}
+
+export function isTextNode(node) {
+  return node && node.nodeType === 3;
+}
