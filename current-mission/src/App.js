81b7ba963a212425d4d0590512516cbc1804686e
Kyeongman Kang <kyeongmankang@gmail.com>
2022-03-11 17:23:20 +0900

    [NP-183] 마크업 제작을 위한 스타일링 관련 패키지 설정 및 적용
    - styled-components를 이용 전역 스타일링
    - reset을 위한 styled-reset 설치

--- current-mission/src/App.js
+++ current-mission/src/App.js
@@ -1,13 +1,31 @@
 import React from 'react';
+import reset from "styled-reset";
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
 import Header from './components/Header';
 import Main from './components/main';
 
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+  * {
+    box-sizing: border-box;
+  }
+`
+
+const theme = {
+
+}
+
 function App() {
   return (
-    <>
-      <Header />
-      <Main />
-    </>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+        <Header />
+        <Main />
+    </ThemeProvider>
   )
 }
 
